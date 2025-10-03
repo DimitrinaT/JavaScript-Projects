5fca86b3880ba3dff4ed909b340145d5ed019514
DimitrinaT <110131665+DimitrinaT@users.noreply.github.com>
2025-10-03 17:02:18 +0100

    Added Project assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+//using window.alert method
+window.alert("Hello, world!");
+
+//using document.write method
+document.write("Hello, world! ");
+
+// Declare two string variables
+var Sent1 = "This is the beginning of the string"
+var Sent2 = " and this is the end of the string."
+
+// Concatenating the string variables
+document.write(Sent1 + Sent2 + "<br>");
+
+// An expression
+document.write(10 + 8);
