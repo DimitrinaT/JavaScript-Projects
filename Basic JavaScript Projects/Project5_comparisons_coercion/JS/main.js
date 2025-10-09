740e898fb8a17254654ca004811a9e6be033c4b8
DimitrinaT <110131665+DimitrinaT@users.noreply.github.com>
2025-10-09 20:43:12 +0100

    added Infinity assignment to Project5

--- Basic JavaScript Projects/Project5_comparisons_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_coercion/JS/main.js
@@ -1,3 +1,7 @@
 document.write(typeof 9); // it shows the data type of the value
 
 document.write("Name" + 4); // it concatenates a string and a number
+
+document.getElementById("positiveInfinity").innerHTML = 4E308 // it shows positive 'Infinity'
+
+document.getElementById("negativeInfinity").innerHTML = -2E310 // it shows negative 'Infinity'
